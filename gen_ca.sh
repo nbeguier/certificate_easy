c9cb73dd0e3566281ca12bd250fcfbe38031349f
Nicolas Beguier <nicolas.beguier@schibsted.com>
2017-08-28 15:45:10 +0200

    Add opts in bash scripts

--- gen_ca.sh
+++ gen_ca.sh
@@ -3,14 +3,49 @@
 CA_CN='TestCA'
 # The organization of the subject.
 CA_O='AutoSign'
+CA_VALIDITY=365
+OUTPUT=$PWD
+
+while [[ $# -gt 1 ]]
+do
+key="$1"
+
+case $key in
+    -c|--cn)
+    CA_CN="$2"
+    shift # past argument
+    ;;
+    -o)
+    CA_O="$2"
+    shift # past argument
+    ;;
+    -O|--output)
+    OUTPUT="$2"
+    shift # past argument
+    ;;
+    -v|--validity)
+    CA_VALIDITY="$2"
+    shift # past argument
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+
+CA_CN="${CA_CN}"
+# The organization of the subject.
+CA_O="${CA_O}"
 # The country of the subject. Two letter code.
 CA_C='FR'
 # In how many days, counting from today, this certificate will expire.
-CA_VALIDITY=365
+CA_VALIDITY="${CA_VALIDITY}"
 CA_TEMPLATE=.ca.tmp
+OUTPUT="${OUTPUT}"
 
 # Génération de la clé privée de la CA
-openssl req -x509 -newkey rsa:4096 -keyout ca.key -nodes -subj "/CN=${CA_CN}/O=${CA_O}/C=${CA_C}"
+openssl req -x509 -newkey rsa:4096 -keyout "${OUTPUT}"/ca.key -nodes -subj "/CN=${CA_CN}/O=${CA_O}/C=${CA_C}"
 
 # Création du template CA
 echo "
@@ -23,6 +58,6 @@ cert_signing_key
 crl_signing_key" > ${CA_TEMPLATE}
 
 # Génération de la CA
-certtool --generate-self-signed --load-privkey ca.key --template ${CA_TEMPLATE} --outfile ca.pem
+certtool --generate-self-signed --load-privkey "${OUTPUT}"/ca.key --template ${CA_TEMPLATE} --outfile "${OUTPUT}"/ca.pem
 
 rm ${CA_TEMPLATE}
--- gen_certificate.sh
+++ gen_certificate.sh
@@ -3,17 +3,55 @@
 SRV_CN='nicolas.bluesbox.net'
 # The organization of the subject.
 SRV_O='AutoSign'
+# In how many days, counting from today, this certificate will expire.
+SRV_VALIDITY=364
+
+OUTPUT=$PWD
+
+while [[ $# -gt 1 ]]
+do
+key="$1"
+
+case $key in
+    -c|--cn)
+    SRV_CN="$2"
+    shift # past argument
+    ;;
+    -o)
+    SRV_O="$2"
+    shift # past argument
+    ;;
+    -O|--output)
+    OUTPUT="$2"
+    shift # past argument
+    ;;
+    -v|--validity)
+    SRV_VALIDITY="$2"
+    shift # past argument
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+
+SRV_CN="${SRV_CN}"
+# The organization of the subject.
+SRV_O="${SRV_O}"
 # The country of the subject. Two letter code.
 SRV_C='FR'
 # In how many days, counting from today, this certificate will expire.
-SRV_VALIDITY=364
+SRV_VALIDITY="${SRV_VALIDITY}"
 SRV_TEMPLATE=.srv.tmp
 
-CA_PUB_PATH='ca.pem'
-CA_PRIV_PATH='ca.key'
+OUTPUT="${OUTPUT}"
+
+CA_PUB_PATH="${OUTPUT}/ca.pem"
+CA_PRIV_PATH="${OUTPUT}/ca.key"
 
 # Génération de la clé privée du serveur
-openssl req -x509 -newkey rsa:4096 -keyout server.key -nodes -subj "/CN=${SRV_CN}/O=${SRV_O}/C=${SRV_C}"
+openssl req -x509 -newkey rsa:4096 -keyout "${OUTPUT}"/server.key -nodes -subj "/CN=${SRV_CN}/O=${SRV_O}/C=${SRV_C}"
 
 # Création du template serveur
 echo "
@@ -31,8 +69,7 @@ ipsec_ike_key" > ${SRV_TEMPLATE}
 
 # # Génération du certificat du serveur
 certtool --generate-certificate --template ${SRV_TEMPLATE} --load-ca-certificate ${CA_PUB_PATH} \
-  --load-ca-privkey ${CA_PRIV_PATH} --load-privkey server.key --outfile server.pem
+  --load-ca-privkey ${CA_PRIV_PATH} --load-privkey "${OUTPUT}"/server.key --outfile "${OUTPUT}"/server.pem
 
 # Netttoyage
 rm ${SRV_TEMPLATE}
-
